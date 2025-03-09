9f3dfedb3fa4ccb6f9a6ce136e44c550d8f5fe48
Zachary Davis <test106437@gmail.com>
2025-03-09 23:37:13 +0200

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -31,37 +31,11 @@ class AdminCommands(commands.Cog):
             await self.bot.sync_application_commands()
             await ctx.send(f"Done.")
 
-    @nextcord.slash_command(name="draw_lottery", description="(Admin command) Draw the lottery.")
-    async def draw_lottery(self, ctx):
-        if ctx.user.id in self.admins:
-            winner_id, winnings = await self.db.draw_lottery()
-            await ctx.send(f"🍬🍬🍬 <@{winner_id}> has won {winnings} candy! 🍬🍬🍬")
-
-    @nextcord.slash_command(name="candy_injection", description="(Admin command) Candy injection.")
-    async def candy_injection(self, ctx, amount):
-        if ctx.user.id in self.admins:
-            await self.db.add_or_update_user(ctx.user.id, candy=amount)
-            await ctx.send(f"Injection {amount} candy.")
-
-    @nextcord.slash_command(name="backup", description="(Admin command) Backup the database")
-    async def backup(self, ctx):
+    @nextcord.slash_command(name="backup_db", description="(Admin command) Backup the database")
+    async def backup_db(self, ctx):
         if ctx.user.id in self.admins:
             channel = self.bot.get_channel(1312055600062005298)
             await channel.send(file=nextcord.File('main.db'))
-            await ctx.send("Done.")
-
-    @nextcord.slash_command(name="info", description="(Admin command) Database info of a user.")
-    async def info(self, ctx, member):
-        if ctx.user.id in self.admins:
-            user = await self.db.get_user_data(member[2:-1])
-            if user == None:
-                user = f"{member[2:-1]} not in DB!"
-            await ctx.send(user)
-
-    @nextcord.slash_command(name="reset", description="(Admin command) Resets everyone's balance to 1000 times their multiplier.")
-    async def reset(self, ctx):
-        if ctx.user.id in self.admins:
-            await self.db.reset_balances()
             await ctx.send("Done.")
 
     @nextcord.slash_command(name="drop_reactions", description="(Admin command) Drop the reactions table.")
@@ -83,4 +57,30 @@ class AdminCommands(commands.Cog):
         if ctx.user.id in self.admins:
             channel = self.bot.get_channel(1312055600062005298)
             await channel.send(file=nextcord.File('economy.db'))
+            await ctx.send("Done.")
+
+    @nextcord.slash_command(name="draw_lottery", description="(Admin command) Draw the lottery.")
+    async def draw_lottery(self, ctx):
+        if ctx.user.id in self.admins:
+            winner_id, winnings = await self.db.draw_lottery()
+            await ctx.send(f"🍬🍬🍬 <@{winner_id}> has won {winnings} candy! 🍬🍬🍬")
+
+    @nextcord.slash_command(name="candy_injection", description="(Admin command) Candy injection.")
+    async def candy_injection(self, ctx, amount):
+        if ctx.user.id in self.admins:
+            await self.db.add_or_update_user(ctx.user.id, candy=amount)
+            await ctx.send(f"Injection {amount} candy.")
+
+    @nextcord.slash_command(name="info", description="(Admin command) Database info of a user.")
+    async def info(self, ctx, member):
+        if ctx.user.id in self.admins:
+            user = await self.db.get_user_data(member[2:-1])
+            if user == None:
+                user = f"{member[2:-1]} not in DB!"
+            await ctx.send(user)
+
+    @nextcord.slash_command(name="reset", description="(Admin command) Resets everyone's balance to 1000 times their multiplier.")
+    async def reset(self, ctx):
+        if ctx.user.id in self.admins:
+            await self.db.reset_balances()
             await ctx.send("Done.")
