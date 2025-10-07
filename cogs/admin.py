67a9a668db0ac57eecd9755158e44f87deb96d3b
3zad <test106437@gmail.com>
2025-10-07 19:34:06 +0300

    unfinished fetch command

--- cogs/admin.py
+++ cogs/admin.py
@@ -11,8 +11,27 @@ class AdminCommands(commands.Cog):
         self.config = config
         self.admins = config["owners"]
 
+        self.fetching = False
+
     @nextcord.slash_command(name="shutdown", description="(Admin command) Turn off the bot")
     async def shutdown(self, ctx):
         if ctx.user.id in self.admins:
             await ctx.send("Shutting down!")
             sys.exit(0)
+
+    @nextcord.slash_command(name="fetch", description="(Admin command) Fetch content of messages for the database.")
+    async def fetch(self, ctx: nextcord.Interaction):
+        if ctx.user.id not in self.admins:
+            await ctx.response.send_message("You do not have permission to mute members!", ephemeral=True)
+            return
+
+        if self.fetching:
+            await ctx.response.send_message("Already fetching messages!", ephemeral=True)
+            return
+        
+        self.fetching = True
+
+        while True:
+            pass
+
+        await ctx.response.send_message("Done fetching messages!", ephemeral=True)
